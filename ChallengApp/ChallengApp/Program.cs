b28ff49a97c36963bc6ff0dbd547b30948be62c5
Lukaszek84 <pan_lukasz_kostek@wp.pl>
2023-04-15 22:49:03 +0200

    zadanie domowe 4

--- ChallengApp/ChallengApp/Program.cs
+++ ChallengApp/ChallengApp/Program.cs
@@ -1,1 +1,17 @@
-﻿Console.WriteLine("Hello, World! Lukasz");
+﻿string personsName = "Lukasz Kostek" ;
+
+int personsAge = 38 ;
+
+bool men = true ;
+
+string result1 = "Meżczyna, w wieku 38 lat.";
+string result2 = "Chodzi o kogoś, innego";
+
+if (personsName == "Lukasz Kostek" && personsAge == 38 && men != false)
+{
+    Console.WriteLine(result1);
+}
+else
+{
+    Console.WriteLine(result2);
+}
